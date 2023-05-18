38b921d983d31d10ae7b61689f203560d7bddae1
MemoryMist <21455538-MemoryMist@users.noreply.replit.com>
2023-05-18 02:56:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+stonks_book = {
+  "JPM": "33.33",
+  "AAPL": "45.40",
+  "ABBV": "17.20",
+  "FENC": "31.11",
+  "ESRT": "12.80",
+  "ESTE": "14.71",
+  "HBAN": "20.05",
+  "KMDA": "64.30",
+  "MOXC": "15.90",
+  "MLSS": "8.34",
+  "REGN": "20.20"
+}
+x = input('Enter Ticker Symbol Here: ')
+z =stonks_book.get(x,'No Stonks. Ticker not found.')
+print(z)
